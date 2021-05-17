3f1e0628f016d7980445e665be45f8cca9fb90b9
viktor_vrubleuski <viktorvrubleuski@gmail.com>
2021-05-17 21:11:30 +0300

    remove content type

--- static/js/calculating_taxes.js
+++ static/js/calculating_taxes.js
@@ -5,9 +5,8 @@ $(document).ready(function () {
 
         $.ajax({
             url: 'api/calculate-tax/',
-            data: JSON.stringify(serializedData),
+            data: serializedData,
             type: 'post',
-            contentType: "application/json",
             success: function(response) {
                 if(response.data.detail == true) {
                     $("#taxDetail").html('<div class="card mb-1"><div class="card-body"><ul class="list-group mb-3"><li class="list-group-item d-flex justify-content-between lh-sm"><div><h6 class="my-0">0% on earnings up to £12,500</h6></div><span class="text-muted">£0</span></li><li class="list-group-item d-flex justify-content-between lh-sm"><div><h6 class="my-0">20% on any earnings between £12,501 and £50,000</h6></div><span class="text-muted">£' + response.data.income_tax_slab_1 + '</span></li><li class="list-group-item d-flex justify-content-between lh-sm"><div><h6 class="my-0">40% on any earnings between £50,001 and £150,000</h6></div><span class="text-muted">£' + response.data.income_tax_slab_2 + '</span></li><li class="list-group-item d-flex justify-content-between lh-sm"><div><h6 class="my-0">45% on earnings of £150,001 and over</h6></div><span class="text-muted">£' + response.data.income_tax_slab_3 + '</span></li><li class="list-group-item d-flex justify-content-between"><span>Total tax (LBS)</span><strong>£' + response.data.tax_result + '</strong></li></ul>');
